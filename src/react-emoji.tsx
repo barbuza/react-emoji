68364acb28d96d7ea0ff5e931edb827c0c41f92e
Shakhov Egor <gorpacrate@gmail.com>
2017-08-23 18:05:43 +0300

    chidren arent always of string type

--- src/react-emoji.tsx
+++ src/react-emoji.tsx
@@ -87,8 +87,16 @@ export interface IReactEmojiProps {
   tag?: string;
 }
 
-export const ReactEmoji: React.SFC<IReactEmojiProps> = (props) => React.createElement(
-  props.tag || "div",
-  props.attrs,
-  ...emojifyText(props.children, props),
-);
+export const ReactEmoji: React.SFC<IReactEmojiProps> = (props) => {
+  const children = React.Children.map(props.children, (child) => {
+    if (typeof child === "string") {
+      return emojifyText(child, props);
+    }
+    return child;
+  });
+  return React.createElement(
+    props.tag || "div",
+    props.attrs,
+    ...children,
+  );
+};
