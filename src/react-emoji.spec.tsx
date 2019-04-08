6a762a61ea5d8506bb717a6af7c40b02cc3a80c4
Victor Kotseruba <victor@tutorme.com>
2019-04-09 01:39:21 +0300

    lint

--- src/react-emoji.spec.tsx
+++ src/react-emoji.spec.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+// tslint:disable-next-line: no-implicit-dependencies no-submodule-imports
 import { renderToStaticMarkup } from "react-dom/server";
 
 import { EmojiType, ReactEmoji } from "./react-emoji";
